3adb968479766a7118b79e47cf58b7cbaa8029bb
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-02-02 14:13:09 -0300

    Creacion de Hook propio

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import useSelectMonedas from '../hooks/useSelectMonedas';
+import { monedas } from '../data/monedas';
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
@@ -12,6 +14,7 @@ const InputSubmit = styled.input`
     font-size:20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover{
         background-color: #7a7dfe;
@@ -20,8 +23,17 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+
+
+
+    const [ moneda, SelectMonedas] = useSelectMonedas('Elije tu Moneda', monedas)
+    
+
     return (
         <form>
+
+            <SelectMonedas/>
+
             <InputSubmit
                 type='submit'
                 value='Cotizar'
